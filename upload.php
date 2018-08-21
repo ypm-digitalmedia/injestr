d5877e41f781ddf9aff303c6fb060706f922943b
Andy Melien <ypm-digitalmedia@gmail.com>
2018-08-21 11:42:13 -0400

    error checking

--- upload.php
+++ upload.php
@@ -13,7 +13,35 @@ if (!empty($_FILES)) {
      
     $targetFile =  $targetPath. $_FILES['file']['name'];  //5
  
-    move_uploaded_file($tempFile,$targetFile); //6
+//    move_uploaded_file($tempFile,$targetFile); //6
      
+	
+	if ( move_uploaded_file ($tempFile,$targetFile)  )
+      {  echo 'The file ' . $_FILES['file']['name'] . ' has been successfully uploaded.';
+       }
+else
+      {
+        switch ($_FILES['uploadFile'] ['error'])
+         {  case 1:
+                   echo 'ERROR: The file is bigger than this PHP installation allows.';
+                   break;
+            case 2:
+                   print 'ERROR: The file is bigger than this form allows';
+                   break;
+            case 3:
+                   print 'ERROR: Only part of the file was uploaded';
+                   break;
+            case 4:
+                   print 'ERROR: No file was uploaded';
+                   break;
+         }
+       }
+	
+	
+	
+	
+	
+	
+	
 }
-?>      
+?>
