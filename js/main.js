f0d001926d2372785e396179c361a6e905d32f10
Andy Melien <ypm-digitalmedia@gmail.com>
2018-11-06 13:55:22 -0500

    hotfix - change chunksize to 1.5gb

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
 		autoQueue: true, //if false, files added to the dropzone will not be queued by default.
 		capture: null, //null|camera|microphone|camcorder.  multiple=false for apple devices
 		chunking: true,
-		chunkSize: 700000000, //bytes
+		chunkSize: 1500000000, //bytes
 		clickable: true,
 		createImageTHumbnails: true,
 		createImageThumbnails: true,
